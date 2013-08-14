681deb51d5d3ef5ecae441a52ec3425f600ff814
Dave Abrahams <dave@boostpro.com>
2013-08-14 05:20:09 -0700

    Stop creating the dumpfile by default
    it was taking up too much space on the ci server

--- update.py
+++ update.py
@@ -6,34 +6,40 @@
 
 from util import svn_head_revision as head_rev
 from paths import *
-import subprocess, os
+import subprocess, os, sys
 
-old_head = head_rev(local_repo_url)
-try:
-    print ' === syncing local SVN repo ==='
-    subprocess.check_call(
-        ['svnsync', 'synchronize', '--non-interactive', local_repo_url])
-
-    # If this fails, we'll rebuild the dump from scratch
-    open(dump_file, 'r')
-
-    if head_rev(local_repo_url) == old_head:
-        print ' === Sync triggered, but found nothing to do === '
-    else:
-        print ' === updating dumpfile ==='
-        subprocess.check_call(
-            ['svnadmin', 'dump', local_repo_dir, '-r', str(old_head), '--incremental'],
-            stdout=open(dump_file, 'ab'))
-except:
-    # If anything went wrong, try to rebuild the dump file from the
-    # beginning
+def run(dump = False):
+    old_head = head_rev(local_repo_url)
     try:
-        print ' === incremental update failed; rebuilding dumpfile... ==='
+        print ' === syncing local SVN repo ==='
         subprocess.check_call(
-            ['svnadmin', 'dump', '--quiet', local_repo_dir], 
-            stdout=open(dump_file, 'wb'))
+            ['svnsync', 'synchronize', '--non-interactive', local_repo_url])
+
+        if !dump: return
+
+        # If this fails, we'll rebuild the dump from scratch
+        open(dump_file, 'r')
+
+        if head_rev(local_repo_url) == old_head:
+            print ' === Sync triggered, but found nothing to do === '
+        else:
+            print ' === updating dumpfile ==='
+            subprocess.check_call(
+                ['svnadmin', 'dump', local_repo_dir, '-r', str(old_head), '--incremental'],
+                stdout=open(dump_file, 'ab'))
     except:
-        if os.path.exists(dump_file):
-            print ' === removing failed dump results ==='
-            os.unlink(dump_file)
-        raise # If that doesn't work either, finally, really fail.
+        # If anything went wrong, try to rebuild the dump file from the
+        # beginning
+        try:
+            print ' === incremental update failed; rebuilding dumpfile... ==='
+            subprocess.check_call(
+                ['svnadmin', 'dump', '--quiet', local_repo_dir], 
+                stdout=open(dump_file, 'wb'))
+        except:
+            if os.path.exists(dump_file):
+                print ' === removing failed dump results ==='
+                os.unlink(dump_file)
+            raise # If that doesn't work either, finally, really fail.
+
+if __name__ == '__main__':
+    run('--dump' in sys.argv)
